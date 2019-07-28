ed7b2cbff674d7f1b0b36118d8e34b952077ab69
Viswesvar Sekar <v.sekar@mytaxi.com>
2019-07-28 21:07:23 +0200

    feat: initial commit

---
+++ src/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to the star wars character wiki</h1>
+                <p>Type and search your favourite characters</p>
+            </div>
+        );
+    }
+}
+
+export default Header;
